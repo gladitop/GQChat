01bb58878f47ff2306a8db8db1ead39757cfaf71
damiralmaev <damiralmaev@gmail.com>
2020-03-31 20:18:09 +0300

    Начало

---
+++ Server/Server/Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Server
+{
+    static public class Data
+    {
+        static public object Settings { get; set; }//Настройки
+
+        //Прочее
+
+        public struct ClientConnectOnly//инфа о клиенте (онлайн)
+        {
+            public Socket ClientSocket;
+            public int ID;
+        }
+
+        public struct ClientConnectOffline//инфа о клиенте (офнлайн)
+        {
+            public int ID;
+            public string Email;
+            public string Passworld;
+        }
+
+        public struct GeneralChatMess//Сообщение для клиента (Общий чат)
+        {
+            public int ID;
+            public string Text;
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Server
+{
+    class Program
+    {
+        static Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static StreamWriter sw = new StreamWriter("Log.txt");
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            WriteLine("Загрузка сервера...", ConsoleColor.Yellow);
+            sw.AutoFlush = true;
+            server.Bind(new IPEndPoint(IPAddress.Any, 908));
+            server.Listen(99999);
+
+            Thread thread = new Thread(new ThreadStart(NewConnect));
+            thread.IsBackground = true;
+            thread.Start();
+
+            Console.ReadLine();
+            sw.Close();
+            Environment.Exit(0);
+        }
+
+        static void CheckNewConnect(object i)
+        {
+            byte[] buffer = new byte[1024];
+            Socket client = (Socket)i;
+
+            int messi = client.Receive(buffer);
+
+            //Провека проги
+            if (Encoding.UTF8.GetString(buffer, 0, messi) != "TCPCHAT 1.0")
+            {
+                WriteLine("Ошибка: Cтарый или другой клиент!", ConsoleColor.Red);
+                client.Close();
+                return;//Проверить!
+            }
+
+            //Команды
+
+            while (true)
+            {
+                Task.Delay(10).Wait();
+
+                messi = client.Receive(buffer);
+
+                if (Encoding.UTF8.GetString(buffer) == "REG")//регистрация
+                {
+                    //email
+
+                    //пароль
+                }
+                else if (Encoding.UTF8.GetString(buffer) == "LOG")//Вход
+                {
+
+                }
+                else if (Encoding.UTF8.GetString(buffer) == "MES")//Сообщение
+                {
+
+                }
+            }
+        }
+
+        static void NewConnect()
+        {
+            while (true)
+            {
+                try
+                {
+                    Socket client = server.Accept();
+                    Thread thread = new Thread(new ParameterizedThreadStart(CheckNewConnect));
+                    thread.Start(client);
+                }
+                catch { WriteLine("Ошибка клиента!", ConsoleColor.Red); }
+            }
+        }
+
+        static void WriteLine(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            sw.WriteLine($"{DateTime.Now}: {text}");
+        }
+    }
+}
