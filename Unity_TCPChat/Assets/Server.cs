545d548732137c69af5cc50e650fc18316cc37f9
Qliook <QliookdoijnoGT@yandex.ru>
2020-04-01 14:22:23 +0300

    Unity Chat
    проект unity pre-alfa-pre-alfa v 0.5

---
+++ Unity_TCPChat/Assets/Server.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System;
+using System.Net;
+using System.IO;
+
+public class Server : MonoBehaviour
+{
+    private List<ServerClient> clients;
+    private List<ServerClient> disconnectlist;
+
+    public int port = 6321;
+    private TcpListener server;
+    private bool serverStarted;
+
+    private void Start()
+    {
+        clients = new List<ServerClient>();
+        disconnectlist = new List<ServerClient>();
+
+        try
+        {
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            StartListening();
+            serverStarted = true;
+            Debug.Log("Server has been started on port " + port.ToString());
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    private void Update()
+    {
+        if (!serverStarted)
+            return;
+
+        foreach(ServerClient c in clients)
+        {
+            // is the clientrs still connected?
+            if(!IsConnected(c.tcp))
+            {
+                c.tcp.Close();
+                disconnectlist.Add(c);
+                continue;
+            }
+            // check message from the client
+            else
+            {
+                NetworkStream s = c.tcp.GetStream();
+                if(s.DataAvailable)
+                {
+                    StreamReader reader = new StreamReader(s, true);
+                    string data = reader.ReadLine();
+
+                    if (data != null)
+                        OnIncomingData(c, data);
+                }
+            }
+        }
+
+        for(int i = 0; i < disconnectlist.Count - 1; i++)
+        {
+            Broadcast(disconnectlist[i].clientName + " has disconnected", clients);
+
+            clients.Remove(disconnectlist[i]);
+            disconnectlist.RemoveAt(i);
+        }
+    }
+
+    private void StartListening()
+    {
+        server.BeginAcceptTcpClient(AcceptTcpClient, server);
+    }
+    private bool IsConnected(TcpClient c)
+    {
+        try
+        {
+            if(c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                {
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+                }
+
+                return true;
+            }
+            else
+                return false;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    private void AcceptTcpClient(IAsyncResult ar)
+    {
+        TcpListener listener = (TcpListener)ar.AsyncState;
+
+        clients.Add(new ServerClient(listener.EndAcceptTcpClient(ar)));
+        StartListening();
+
+        //Send a message to everyone, say has connected
+        Broadcast("%NAME",new List<ServerClient>() { clients[clients.Count - 1] });
+    }
+
+    private void OnIncomingData (ServerClient c, string data)
+    {
+        if(data.Contains("&NAME"))
+        {
+            c.clientName = data.Split('|')[1];    
+            Broadcast(c.clientName + " has connected", clients);
+            return;
+        }
+
+        Broadcast(c.clientName + " : " + data,clients);
+    }
+    private void Broadcast(string data, List<ServerClient> cl)
+    {
+        foreach(ServerClient c in cl)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(c.tcp.GetStream());
+                writer.WriteLine(data);
+                writer.Flush();
+            }
+            catch(Exception e)
+            {
+                Debug.Log("Write error : " + e.Message + " to client " + c.clientName);
+            }
+        }
+    }
+}
+
+public class ServerClient
+{
+    public TcpClient tcp;
+    public string clientName;
+
+    public ServerClient(TcpClient clientSocket)
+    {
+       // clientName = "Guest";
+        tcp = clientSocket;
+    }
+}
