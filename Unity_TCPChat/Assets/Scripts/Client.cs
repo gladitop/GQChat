9bd2adcf268ebac78f64317a24635abde7a83c62
Qliook <QliookdoijnoGT@yandex.ru>
2020-04-01 23:34:15 +0300

    MiniChanges

---
+++ Unity_TCPChat/Assets/Scripts/Client.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Net.Sockets;
+using System.IO;
+using UnityEngine.UI;
+using System;
+
+public class Client : MonoBehaviour
+{
+    public GameObject chatContainer;
+    public GameObject messagePrefab;
+
+    public string clientName;
+
+    private bool socketReady;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamWriter writer;
+    private StreamReader reader;
+
+    public void ConnectedToServer()
+    {
+        //if already connection, igmore this function
+        if (socketReady)
+            return;
+
+        //Defalt host / post values
+        string host = "127.0.0.1";
+        int port = 908;
+
+        // Overwrite default host / port values, if there is something in those boxes
+        string h;
+        int p;
+        h = GameObject.Find("HostInput").GetComponent<InputField>().text;
+        if (h != "")
+            host = h;
+        int.TryParse(GameObject.Find("PortInput").GetComponent<InputField>().text, out p);
+        if (p != 0)
+            port = p;
+
+        //Create the socket
+        try
+        {
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+            socketReady = true;
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Socket error : " + e.Message);
+        }
+    }
+
+    private void Update()
+    {
+        if(socketReady)
+        {
+            if(stream.DataAvailable)
+            {
+                string data = reader.ReadLine();
+                if (data != null)
+                    OnIncomingData(data);
+            }
+        }
+    }
+
+    private void OnIncomingData(string data)
+    {
+        if (data == "%NAME")
+        {
+            Send("&NAME|" + clientName);        
+            return;
+        }
+
+        GameObject go = Instantiate(messagePrefab, chatContainer.transform);
+        go.GetComponentInChildren<Text>().text = data;
+        Destroy(go, 10);
+    }
+
+    private void Send(string data)
+    {
+        if (!socketReady)
+            return;
+
+        writer.WriteLine(data);
+        writer.Flush();
+    }
+
+    public void OnSendButton()
+    {
+        string message = GameObject.Find("SendInput").GetComponent<InputField>().text;
+        GameObject.Find("SendInput").GetComponent<InputField>().text = "";
+        Send(message);
+    }
+
+    private void CloseSocket()
+    {
+        if (!socketReady)
+            return;
+
+        writer.Close();
+        reader.Close();
+        socket.Close();
+        socketReady = false;
+    }
+
+    private void OnApplicationQuit()
+    {
+        CloseSocket();
+    }
+
+    private void OnDisable()
+    {
+        CloseSocket();
+    }
+}
+ 
