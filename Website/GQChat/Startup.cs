ac04ecdb5a4b5de48ad2d67da432447be99ec927
damiralmaev <damiralmaev@gmail.com>
2020-04-13 21:00:21 +0300

    Где тут CSS

--- Website/GQChat/Startup.cs
+++ Website/GQChat/Startup.cs
@@ -16,14 +16,23 @@ namespace GQChat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
-
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapRazorPages();
+            });
         }
     }
 }
