8323c5fb05d9c85ed3fb4e2c8c7592a22f230a03
damiralmaev <damiralmaev@gmail.com>
2020-04-13 18:02:23 +0300

    Теперь есть C#!

--- Website/GQChat/Startup.cs
+++ Website/GQChat/Startup.cs
@@ -21,23 +21,9 @@ namespace GQChat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                /*
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-                */
-            });
         }
     }
 }
